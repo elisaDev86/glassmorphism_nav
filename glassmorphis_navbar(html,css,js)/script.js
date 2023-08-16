263a54e7c795ee898d634a96b249df4659d48971
ElisaDev <140317344+elisaDev86@users.noreply.github.com>
2023-08-16 14:59:53 +0200

    Add files via upload

---
+++ glassmorphis_navbar(html,css,js)/script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('#menu-icon');
+
+const navbar = document.querySelector('.navbar');
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
